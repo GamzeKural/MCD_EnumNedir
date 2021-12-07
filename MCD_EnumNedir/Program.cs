3183cc663663a98051408c783ac1bb3f550d87a9
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-07 17:00:18 +0300

    Add project files.

---
+++ MCD_EnumNedir/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EnumNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            M1.id = 1;
+            M1.musteriNumara = "MSN001";
+            M1.isim = "Gamze";
+            M1.soyisim = "Kural";
+            M1.emailAdres = "GamzeKural98@gmail.com";
+
+            SanalDataBase SB = new SanalDataBase();
+            musteriReturnValue musteriKayitSonuc = SB.musteriYeniKayit(M1);
+
+            int enumInt = (int)musteriReturnValue.kayitBasarili;
+            Console.WriteLine(enumInt);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MCD_EnumNedir/SanalDataBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_EnumNedir
+{
+    public class SanalDataBase
+    {
+        ArrayList sanalDB = new ArrayList();
+
+        
+
+        public musteriReturnValue musteriYeniKayit(Musteri m)
+        {
+            //parametre kontrol
+            //kayıt kontrol
+
+            sanalDB.Add(m);
+            return musteriReturnValue.kayitBasarili;
+        }
+    }
+}
